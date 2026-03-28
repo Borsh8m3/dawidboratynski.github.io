b054af58abd8f59284887d815f802c2261cb7c5d
Dawid Boratyński <268094@student.pwr.edu.pl>
2026-03-28 18:47:19 +0100

    all

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Meal_planer/', // TO JEST KLUCZOWE: nazwa Twojego repozytorium
+  base: '/Meal_planer/',
 })
